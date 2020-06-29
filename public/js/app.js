01929562ab1abc023f68ba510b2a26e34b2738af
Damanvir Singh <damanveer.singh@hoolime.co>
2020-06-29 22:32:20 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+console.log('Client Side javascript File is loaded!')
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//   response.json().then((data) => {
+//     console.log(data)
+//   })
+// })
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+weatherForm.addEventListener('submit', function (e) {
+  e.preventDefault()
+  
+  const location = search.value
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
+  
+  fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error
+      } else {
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecast
+      }    
+    })
+  })
+  console.log(location)
+})
